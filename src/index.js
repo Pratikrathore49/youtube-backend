7962c41d63aa5f5cf6072e63295a2f11e8f74675
Pratik Rathore <jipratik49@gmail.com>
2026-01-11 20:38:12 +0530

    feat/apiError and apiResponce

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import 'dotenv/config'
 import connectDB from "./db/index.js";
+import { app } from './app.js';
+const PORT =process.env.PORT || 8000
 
 connectDB()
+.then(()=>{
+  app.listen(PORT,()=>{
+    console.log(`Server is running at http://localhost:${PORT}`)
+  })
+})
+.catch((err)=>{
+  console.log('MONGO db Connection failed !!!',err)
+})
 
 
 
---
+++ utils/asyncHandler.js
@@ -0,0 +1,33 @@
+const asyncHanler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((error) =>
+      next(error)
+    );
+  };
+};
+
+
+
+export {asyncHanler}
+
+
+
+
+
+
+
+
+
+
+
+
+// const asyncHanler = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (error) {
+//     res.status(error.code || 5000).json({
+//       success: false,
+//       message: error.message,
+//     });
+//   }
+// };
